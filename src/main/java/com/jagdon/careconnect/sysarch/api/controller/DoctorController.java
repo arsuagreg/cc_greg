1992507c653a5ac93be4d99d37d5a90a02a6d1f1
Greg <margreg982@gmail.com>
2024-07-18 22:00:44 +0800

    CORS Issues

--- src/main/java/com/jagdon/careconnect/sysarch/api/controller/DoctorController.java
+++ src/main/java/com/jagdon/careconnect/sysarch/api/controller/DoctorController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jagdon.careconnect.sysarch.api.model.Doctor;
 import com.jagdon.careconnect.sysarch.service.DoctorService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/doctors")
 public class DoctorController {
